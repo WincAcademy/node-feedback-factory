29abc0a82d6062329c71cb5159b9f6fe1f011017
Niels Bom <niels@nielsbom.com>
2020-03-14 15:13:37 +0100

    Simplify some stuff

--- src/controllers/feedback.controller.js
+++ src/controllers/feedback.controller.js
@@ -1,10 +1,11 @@
-const gitService = require("../services/git.service");
+const gitDownloadService = require("../services/git_download.service");
 const fileService = require("../services/file.service");
 const feedbackService = require("../services/feedback.service");
 
 const getFeedback = async (req, res) => {
   try {
-    const repo = await gitService.getRepository(req.query.repo);
+    // const repo = await gitService.getRepository(req.query.repo);
+    const repo = await gitDownloadService.getRepository(req.query.repo);
     const files = await fileService.getFiles(repo.path);
     const feedback = await feedbackService.getFeedback(files);
     console.log("Retrieved feedback for", repo);
--- src/services/file.service.js
+++ src/services/file.service.js
@@ -16,6 +16,7 @@ const defaultCriteria = {
  */
 async function getFiles(dir, criteria = {}) {
   const options = Object.assign({}, defaultCriteria, criteria);
+  console.log("dir", dir);
   const dirents = await readdir(dir, { withFileTypes: true });
   const files = await Promise.all(
     dirents.map(dirent => {
