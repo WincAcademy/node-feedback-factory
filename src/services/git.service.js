95395c2c792a22a0e066183c167e01de36a44cd7
Jason Koolman <jason.koolman@hotmail.com>
2020-04-02 16:49:49 +0200

    refactor: Create slug once, persist in cache

--- src/services/git.service.js
+++ src/services/git.service.js
@@ -6,7 +6,7 @@ const CACHE = {}; // lazy cache using a hashmap
 
 const hasRepoCached = repo => CACHE.hasOwnProperty(repo);
 const getFromCache = repo => CACHE[repo];
-const cacheRepo = (slug, path, repo) => (CACHE[slug] = { slug, path, repo });
+const cacheRepo = (slug, user, repo, path) => (CACHE[slug] = { slug, user, repo, path });
 const getRepoSlug = (user, repo, branch) => `${user}/${repo}@${branch}`;
 
 /**
@@ -19,29 +19,30 @@ const getRepoSlug = (user, repo, branch) => `${user}/${repo}@${branch}`;
  */
 async function getRepository(user, repo, branch = 'master') {
   const slug = getRepoSlug(user, repo, branch);
+
   return hasRepoCached(slug)
     ? getFromCache(slug)
-    : await downloadRepository(user, repo, branch);
+    : await downloadRepository(slug, user, repo, branch);
 }
 
 /**
  * Download the contents of the given repository.
  *
+ * @param   {string} slug
  * @param   {string} user
  * @param   {string} repo
  * @param   {string} branch
  * @returns {Promise}
  */
-async function downloadRepository(user, repo, branch = 'master') {
+async function downloadRepository(slug, user, repo, branch = 'master') {
   const path = join(STORAGE_PATH, user, repo);
-  const slug = getRepoSlug(user, repo, branch);
   const zipUrl = `https://github.com/${user}/${repo}/archive/${branch}.zip`;
 
   await download(zipUrl, path, { extract: true });
 
   const targetPath = join(path, `${repo}-${branch}`);
 
-  cacheRepo(slug, targetPath, repo);
+  cacheRepo(slug, user, repo, targetPath);
 
   return getFromCache(slug);
 }
